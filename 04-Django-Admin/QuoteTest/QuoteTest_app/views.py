69a5ec4a27b081e30b99fe3d0ac7f9dbb7ef8eb3
Maisie Campbell <maisie-campbell@hotmail.com>
2017-07-18 13:04:19 +0100

    created get_qutoes view

--- 04-Django-Admin/QuoteTest/QuoteTest_app/views.py
+++ 04-Django-Admin/QuoteTest/QuoteTest_app/views.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from QuoteTest_app.models import Quote
 
 # Create your views here.
+def get_quotes(request):
+	return render(request, "QuoteTest/home.html",
+		{'quote_list': Quote.objects.all()})
