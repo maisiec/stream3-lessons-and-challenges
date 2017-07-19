b4144e0834432e9bfa944cc2bdaae7bb4252811f
Maisie Campbell <maisie-campbell@hotmail.com>
2017-07-19 14:18:14 +0100

    ammended url patterns

--- 05-Django-Blog/blog_prj/Blog/urls.py
+++ 05-Django-Blog/blog_prj/Blog/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^blog/$', views.post_list),
-    url(r'^blog/(?P<id>\d+)/$', views.post_detail),
+	url(r'^blog/$', views.post_list),
+	url(r'^blog/(?P<id>\d+)/$', views.post_details),
 ]
