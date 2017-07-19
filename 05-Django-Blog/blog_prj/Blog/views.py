ebb3f7b5add2576fcc8bda4f0d552fbd51293919
Maisie Campbell <maisie-campbell@hotmail.com>
2017-07-19 12:45:25 +0100

    create post detail view using id to fetch model and bind to postdetil

--- 05-Django-Blog/blog_prj/Blog/views.py
+++ 05-Django-Blog/blog_prj/Blog/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -15,4 +15,14 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()
         ).order_by('-published_date')
     return render(request, "blogposts.html", {'posts': posts})
-	
+
+def post_detail(request, id):
+	"""
+	Create a view that return a signe
+	Post object based on the post ID and
+	render it to the 'postdetail.html'
+	template. Or return a 404 error if 
+	the post is not found
+	"""
+	post = get_object_or_404(Post, pk=id)
+	return render(request, "postdetail.html", {'post': post})
