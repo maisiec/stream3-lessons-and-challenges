d4079f7a320384b0d27da5c2be2448c62d234a7b
Maisie Campbell <maisie-campbell@hotmail.com>
2017-07-20 22:34:29 +0100

    added edit_post view

--- 05-Django-Blog/blog_prj/Blog/views.py
+++ 05-Django-Blog/blog_prj/Blog/views.py
@@ -54,3 +54,17 @@ def new_post(request):
 		form = BlogPostForm()
 	return render(request, 'blogpostform.html', {'form': form})
 
+def edit_post(request, id):
+	post = get_object_or_404(Post, pk=id)
+	if request.method == "POST":
+		form = BlogPostForm(request.POST, request.FILES, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect(post_detail, post.pk)
+	else:
+		form = BlogPostForm(instance=post)
+	return render(request, 'blogpostform.html', {'form': form})
+
