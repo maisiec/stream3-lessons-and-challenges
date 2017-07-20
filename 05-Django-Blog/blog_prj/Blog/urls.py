116847ffcd30e6094ed6c9d6cd3819548e687854
Maisie Campbell <maisie-campbell@hotmail.com>
2017-07-20 21:13:13 +0100

    added new url pattetn to find new_post template

--- 05-Django-Blog/blog_prj/Blog/urls.py
+++ 05-Django-Blog/blog_prj/Blog/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-import views
+from . import views
 
 urlpatterns = [
-	url(r'^blog/$', views.post_list),
-	url(r'^blog/(?P<id>\d+)/$', views.post_details),
-	url(r'^blog/top', views.top_posts)
+	url(r'^blog/$', views.post_list, name='post_list'),
+	url(r'^blog/(?P<id>\d+)/$', views.post_detail),
+	url(r'^blog/top', views.top_posts),
+    url(r'^post/new/$', views.new_post, name='new_post')
 ]
