fa5190932287f471cebbe5fd81da6119fc7124d0
Maisie Campbell <maisie-campbell@hotmail.com>
2017-07-20 22:39:52 +0100

    add edit_post url pattern to link up to the view

--- 05-Django-Blog/blog_prj/Blog/urls.py
+++ 05-Django-Blog/blog_prj/Blog/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
 	url(r'^blog/$', views.post_list, name='post_list'),
 	url(r'^blog/(?P<id>\d+)/$', views.post_detail),
 	url(r'^blog/top', views.top_posts),
-    url(r'^post/new/$', views.new_post, name='new_post')
+    url(r'^post/new/$', views.new_post, name='new_post'),
+    url(r'^blog/(?P<id>\d+)/edit$', views.edit_post, name='edit'),
 ]
