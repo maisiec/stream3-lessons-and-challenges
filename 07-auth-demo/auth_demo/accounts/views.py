85f6a3e9e485279008880ac49e6e2a4322a4db3e
Maisie Campbell <maisie-campbell@hotmail.com>
2017-07-24 17:38:53 +0100

    added log in to views

--- 07-auth-demo/auth_demo/accounts/views.py
+++ 07-auth-demo/auth_demo/accounts/views.py
@@ -4,7 +4,8 @@ from django.contrib import messages, auth
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
 from django.template.context_processors import csrf
-from accounts.forms import UserRegistrationForm
+from accounts.forms import UserRegistrationForm, UserLoginForm
+from django.contrib.auth.decorators import login_required
  
 def register(request):
     if request.method == 'POST':
@@ -29,6 +30,30 @@ def register(request):
     args.update(csrf(request))
  
     return render(request, 'register.html', args)
-    
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            user = auth.authenticate(email=request.POST.get('email'),
+                                    password=request.POST.get('password'))
+ 
+            if user is not None:
+                auth.login(request, user)
+                messages.error(request, "You have successfully logged in")
+                return redirect(reverse('profile'))
+            else:
+                form.add_error(None, "Your email or password was not recognised")
+ 
+    else:
+        form = UserLoginForm()
+ 
+    args = {'form':form}
+    args.update(csrf(request))
+    return render(request, 'login.html', args)
+
+@login_required(login_url='/login/')    
 def profile(request):
     return render(request, 'profile.html')
+
