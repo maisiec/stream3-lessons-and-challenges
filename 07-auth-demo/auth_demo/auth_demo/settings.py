fe98c6366ebfcc109c801753f85693e0e2c53797
Maisie Campbell <maisie-campbell@hotmail.com>
2017-07-24 14:17:47 +0100

    added auth_user_model to settings

--- 07-auth-demo/auth_demo/auth_demo/settings.py
+++ 07-auth-demo/auth_demo/auth_demo/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# To tell Django that we want to use this class as our User class, 
+# we need to add a line to the settings.py:
+AUTH_USER_MODEL = 'accounts.User'
