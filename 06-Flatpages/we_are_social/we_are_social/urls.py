aaf081c5dc585024aa868d8055f8eea415bc9bcf
Maisie Campbell <maisie-campbell@hotmail.com>
2017-07-24 12:14:00 +0100

    configured url routing for flatpages

--- 06-Flatpages/we_are_social/we_are_social/urls.py
+++ 06-Flatpages/we_are_social/we_are_social/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from hello import views
+from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.get_index),
+    url(r'^pages/', include ('django.contrib.flatpages.urls')),
 ]
