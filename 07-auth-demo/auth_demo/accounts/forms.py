00d8e9fcec371be7dc3eb991d994a01a454f345d
Maisie Campbell <maisie-campbell@hotmail.com>
2017-07-25 12:03:12 +0100

    added four new fields to UserRegistration form, for taking credit card details

--- 07-auth-demo/auth_demo/accounts/forms.py
+++ 07-auth-demo/auth_demo/accounts/forms.py
@@ -4,6 +4,20 @@ from accounts.models import User
 from django.core.exceptions import ValidationError 
  
 class UserRegistrationForm(UserCreationForm):
+
+    MONTH_ABBREVIATIONS = [
+        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'June',
+        'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'
+        ]
+    MONTH_CHOICES = list(enumerate(MONTH_ABBREVIATIONS, 1))
+    YEAR_CHOICES =[(i, i) for i in xrange(2015, 2036)]
+
+    credit_card_number = forms.CharField(label='Credit card number')
+    cvv = forms/CharField(label='Security code (CVV)')
+    expiry_month - forms.CharField(label="Month", choices=MONTH_CHOICES)
+    expiry_year = forms.CharField(label="Year", choices=YEAR_CHOICES)
+    stripe_id = forms.CharField(widget=forms.HiddenInput)
+
     password1 = forms.CharField(
         label='Password',
         widget=forms.PasswordInput
@@ -16,7 +30,7 @@ class UserRegistrationForm(UserCreationForm):
  
     class Meta:
         model = User
-        fields = ['email', 'password1', 'password2']
+        fields = ['email', 'password1', 'password2', 'stripe_id']
         exclude = ['username']
  
     def clean_password2(self):
