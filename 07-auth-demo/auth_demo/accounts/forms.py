98abb43cfe4829bbf9f0449291b1478cff4b33f3
Maisie Campbell <maisie-campbell@hotmail.com>
2017-07-25 12:14:48 +0100

    fixed typo

--- 07-auth-demo/auth_demo/accounts/forms.py
+++ 07-auth-demo/auth_demo/accounts/forms.py
@@ -13,9 +13,9 @@ class UserRegistrationForm(UserCreationForm):
     YEAR_CHOICES =[(i, i) for i in xrange(2015, 2036)]
 
     credit_card_number = forms.CharField(label='Credit card number')
-    cvv = forms/CharField(label='Security code (CVV)')
-    expiry_month - forms.CharField(label="Month", choices=MONTH_CHOICES)
-    expiry_year = forms.CharField(label="Year", choices=YEAR_CHOICES)
+    cvv = forms.CharField(label='Security code (CVV)')
+    expiry_month = forms.ChoiceField(label="Month", choices=MONTH_CHOICES)
+    expiry_year = forms.ChoiceField(label="Year", choices=YEAR_CHOICES)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
 
     password1 = forms.CharField(
