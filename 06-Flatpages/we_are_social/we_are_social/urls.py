79393077c41791e921f0e2f080bc79ae3b8f3744
Maisie Campbell <maisie-campbell@hotmail.com>
2017-07-26 12:11:36 +0100

    added products url

--- 06-Flatpages/we_are_social/we_are_social/urls.py
+++ 06-Flatpages/we_are_social/we_are_social/urls.py
@@ -20,6 +20,7 @@ from hello import views
 from django.conf.urls import url, include
 from paypal.standard.ipn import urls as paypal_urls
 from paypal_store import views as paypal_views
+from products import views as product_views
 
 
 urlpatterns = [
@@ -27,6 +28,7 @@ urlpatterns = [
     url(r'^$', views.get_index),
     url(r'^pages/', include('django.contrib.flatpages.urls')),
     url(r'^a-very-hard-to-guess-url/', include(paypal_urls)),
-    url(r'^paypal_return', paypal_views.paypal_return),
-    url(r'^paypal_cancel', paypal_views.paypal_cancel),
+    url(r'^paypal-return', paypal_views.paypal_return),
+    url(r'^paypal-cancel', paypal_views.paypal_cancel),
+    url(r'^products/$', product_views.all_products),
 ]
