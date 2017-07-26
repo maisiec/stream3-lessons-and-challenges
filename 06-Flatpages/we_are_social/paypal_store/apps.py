04269274e60f84532d3be1dba16fd44dffec18a7
Maisie Campbell <maisie-campbell@hotmail.com>
2017-07-26 12:18:22 +0100

    removed data meant for different file

--- 06-Flatpages/we_are_social/paypal_store/apps.py
+++ 06-Flatpages/we_are_social/paypal_store/apps.py
@@ -2,20 +2,5 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render
 
-
-class PaypalStoreConfig(AppConfig):
-    name = 'paypal_store'
-
-@csrf_exempt
-def paypal_return(request):
-	args = {'post': request.POST, 'get':request.GET}
-	return render(request, 'paypal/paypal_return.html', args)
-
-
-def paypal_cancel(request):
-	args = {'post':request.POST, 'get':request.GET}
-	return render(request, 'paypal/paypal_cancel.html', args)
 
