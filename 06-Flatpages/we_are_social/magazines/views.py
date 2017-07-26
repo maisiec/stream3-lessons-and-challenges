35db8572328a8d168454362deaddce4d264bca9f
Maisie Campbell <maisie-campbell@hotmail.com>
2017-07-26 22:57:34 +0100

    added magazines

---
+++ 06-Flatpages/we_are_social/magazines/views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from models import Magazine
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+@login_required
+def all_magazines(request):
+	magazines = Magazine.objects.all()
+	return render(request, "magazines/magazines.html", {"magazines":magazines})
+
+	   
+	
--- 06-Flatpages/we_are_social/we_are_social/urls.py
+++ 06-Flatpages/we_are_social/we_are_social/urls.py
@@ -21,14 +21,28 @@ from django.conf.urls import url, include
 from paypal.standard.ipn import urls as paypal_urls
 from paypal_store import views as paypal_views
 from products import views as product_views
+from accounts.views import register, profile, login, logout, \
+    cancel_subscription, subscriptions_webhook
+from magazines import views as magazine_views
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.get_index),
+
     url(r'^pages/', include('django.contrib.flatpages.urls')),
+    url(r'^register/$', register, name='register'),
+    url(r'^profile/$', profile, name='profile'),
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, name='logout'),
+
+    url(r'^cancel_subscription/$', cancel_subscription, name="cancel_subscription"),
+    url(r'^subscriptions_webhook/$', subscriptions_webhook, name="subscriptions_webhook"),
+
     url(r'^a-very-hard-to-guess-url/', include(paypal_urls)),
     url(r'^paypal-return', paypal_views.paypal_return),
     url(r'^paypal-cancel', paypal_views.paypal_cancel),
+
     url(r'^products/$', product_views.all_products),
+    url(r'^magazines/$', magazine_views.all_magazines),
 ]
