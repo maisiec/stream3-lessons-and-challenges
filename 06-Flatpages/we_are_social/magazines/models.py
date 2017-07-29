465e3462b92b77389cfecafe36fe56233423b164
Maisie Campbell <maisie-campbell@hotmail.com>
2017-07-29 14:55:34 +0100

    imported signals and added valid_ipn_received

--- 06-Flatpages/we_are_social/magazines/models.py
+++ 06-Flatpages/we_are_social/magazines/models.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from signals import subscription_created, subscription_was_cancelled
+from paypal.standard.ipn.signals import valid_ipn_received
 
 # Create your models here.
 class Magazine(models.Model):
@@ -20,3 +22,6 @@ class Purchase(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='purchases')
 	magazine = models.ForeignKey(Magazine)
 	subscription_end = models.DateTimeField(default=timezone.now)
+
+valid_ipn_received.connect(subscription_created)
+valid_ipn_received.connect(subscription_was_cancelled)
