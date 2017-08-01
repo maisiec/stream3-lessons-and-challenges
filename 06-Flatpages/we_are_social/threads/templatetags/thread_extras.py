d90da5ac187f215e705b8395d1fce98c29944018
Maisie Campbell <maisie-campbell@hotmail.com>
2017-08-01 16:13:57 +0100

    add new posts to threads

--- 06-Flatpages/we_are_social/threads/templatetags/thread_extras.py
+++ 06-Flatpages/we_are_social/threads/templatetags/thread_extras.py
@@ -10,3 +10,12 @@ def get_total_subject_posts(subject):
    for thread in subject.threads.all():
        total_posts += thread.posts.count()
    return total_posts
+
+@register.filter
+def started_time(created_at):
+	return arrow.get(created_at).humanize()
+
+@register.simple_tag
+def last_posted_user_name(thread):
+	posts = thread.posts.all().order_by('-created_at')
+	return posts[posts.count()-1].user.username
