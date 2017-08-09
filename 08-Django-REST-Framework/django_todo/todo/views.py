48e448db76d1068845eea3248c4ef3f8d2ea1960
Maisie Campbell <maisie-campbell@hotmail.com>
2017-08-09 16:14:57 +0100

    set up todo app and added serializers

--- 08-Django-REST-Framework/django_todo/todo/views.py
+++ 08-Django-REST-Framework/django_todo/todo/views.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from todo.serializers import TodoSerializer
+from todo.models import Todo
 
 # Create your views here.
+class TodoView(APIView):
+	"""
+	TodoView used to handel the incoming requests relating to 
+	`todo` items
+	"""
+	def get(self, request):
+		"""
+		Retrieve a comple list of `todo` items from the Todo model,
+		serialize them to JSON and return the serialized 
+		todo items
+		"""
+		todo_items = Todo.objects.all()
+		# Serialize the data retrieved from the DBand serialize
+		# them using the `TodoSerializer`
+		serializer = TodoSerializer(todo_items, many=True)
+		# Store the serialized data `serialized_data`
+		serialized_data = serializer.data
+		return Response(serialized_data)
+		
