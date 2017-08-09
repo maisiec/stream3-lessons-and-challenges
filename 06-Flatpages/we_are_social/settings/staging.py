6d16344cc9fd7b653e17b3fae40035e12e8a2e84
Maisie Campbell <maisie-campbell@hotmail.com>
2017-08-09 14:40:14 +0100

    ammendments

--- 06-Flatpages/we_are_social/settings/staging.py
+++ 06-Flatpages/we_are_social/settings/staging.py
@@ -1,4 +1,7 @@
 from base import *
+import dj_database_url
+
+
 
 DEBUG = False
 
@@ -9,11 +12,23 @@ DATABASES = {
     }
 }
 
+# Update database configuration with $DATABASE_URL.
+
+
+# Load the ClearDB connection details from the environment variable
+DATABASES = {
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
+}
+
+
 # Stripe environment variables
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_HCC9iZGSlGKmuv8TeNWFIpaw')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_CrLLB7ikEuQLOlVD3zoRcxs7')
 
 # Paypal Settings
-SITE_URL = 'http://127.0.0.1:8000'
+SITE_URL = 'https://code-institute-social-staging1.herokuapp.com'
 PAYPAL_NOTIFY_URL = 'http://a47f1d9c.ngrok.io/a-very-hard-to-guess-url/'
 PAYPAL_RECEIVER_EMAIL ='maaisiexx-facilitator@hotmail.co.uk'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
